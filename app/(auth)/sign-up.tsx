e4fd1ee69895becb8c334bc27af02a215cab5691
Johannes Schießl <ignotumnomen@icloud.com>
2025-02-08 19:42:32 +0100

    feat(auth): add signup page

---
+++ app/(auth)/sign-up.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+export default function SignUpPage() {
+  return (
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <Text>Sign Up</Text>
+    </SafeAreaView>
+  );
+}
