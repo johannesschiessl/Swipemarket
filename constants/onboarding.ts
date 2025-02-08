d1f33d8654cebaa46702d6403f76f5ac3fefe05b
Johannes Schießl <ignotumnomen@icloud.com>
2025-02-08 19:41:22 +0100

    feat(onboarding): add ONBOARDING_STEPS

---
+++ constants/onboarding.ts
@@ -0,0 +1,27 @@
+export const ONBOARDING_STEPS = [
+  {
+    id: "1",
+    icon: "👋",
+    title: "Explore Unique Items just for you",
+    description: "Find unique items tailored for your tastes every day.",
+  },
+  {
+    id: "2",
+    icon: "💬",
+    title: "Chat & Buy with Confidence",
+    description:
+      "Chat with verified sellers to ask questions and negotiate prices.",
+  },
+  {
+    id: "3",
+    icon: "🔍",
+    title: "Keep Track of Your Favorite Deals",
+    description: "Add items to your wishlist and get notified of price drops.",
+  },
+  {
+    id: "4",
+    icon: "🚀",
+    title: "Reach Interested Buyers Quickly",
+    description: "List your items easily and connect with potential buyers.",
+  },
+];
