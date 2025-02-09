1da74e3cc916d83f4f863b550e4b21085de39452
Johannes Schießl <ignotumnomen@icloud.com>
2025-02-09 14:54:53 +0100

    feat(app): add layout and check for session

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { View, ActivityIndicator } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { supabase } from "@/lib/supabase";
+
+export default function AppLayout(): JSX.Element {
+  const [loading, setLoading] = useState<boolean>(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setLoading(false);
+      if (!session) {
+        router.replace("/(auth)/onboarding");
+      }
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      if (!session) {
+        router.replace("/(auth)/onboarding");
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [router]);
+
+  if (loading) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
+}
