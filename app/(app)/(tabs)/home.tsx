a95607edabf90ca284bdcf7f6ec2344fb9596e48
Johannes Schießl <ignotumnomen@icloud.com>
2025-02-09 14:55:30 +0100

    feat(home): add home page

---
+++ app/(app)/(tabs)/home.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+export default function HomePage() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center">
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+}
