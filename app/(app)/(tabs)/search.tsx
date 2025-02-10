eaf02df0334e4e71bb66b62316cd65802c7f0a25
Johannes Schießl <ignotumnomen@icloud.com>
2025-02-10 20:56:10 +0100

    feat(tabs): update tabs with home, search, create, chat and profile

---
+++ app/(app)/(tabs)/search.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+export default function SearchPage() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center">
+      <Text>Search</Text>
+    </SafeAreaView>
+  );
+}
