a6b491edeeed2914ea6d71e223f98fdf52223319
Johannes Schießl <ignotumnomen@icloud.com>
2025-02-10 21:14:44 +0100

    feat(tabs): update tabs with home, search, create, chat and profile (#12)

--- app/(app)/(tabs)/_layout.tsx
+++ app/(app)/(tabs)/_layout.tsx
@@ -1,4 +1,11 @@
 import { Tabs } from "expo-router";
+import {
+  Search,
+  User,
+  Home,
+  MessagesSquare,
+  PlusCircle,
+} from "lucide-react-native";
 
 export default function TabsLayout() {
   return (
@@ -7,12 +14,39 @@ export default function TabsLayout() {
         name="home"
         options={{
           tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => <Home color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarLabel: "Search",
+          tabBarIcon: ({ color, size }) => <Search color={color} size={size} />,
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          tabBarLabel: "Create",
+          tabBarIcon: ({ color, size }) => (
+            <PlusCircle color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          tabBarLabel: "Chat",
+          tabBarIcon: ({ color, size }) => (
+            <MessagesSquare color={color} size={size} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => <User color={color} size={size} />,
         }}
       />
     </Tabs>
